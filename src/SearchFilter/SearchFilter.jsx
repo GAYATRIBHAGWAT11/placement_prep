730f6c03915a00fa0c1c898743aacd4168af7959
Admin <admin@Admins-MacBook-Air.local>
2023-03-07 17:41:13 +0530

    search filter

--- src/SearchFilter/SearchFilter.jsx
+++ src/SearchFilter/SearchFilter.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+// import { iteratorSymbol } from 'immer/dist/internal';
+import React, { useEffect, useState } from 'react'
 
 const SearchFilter = () => {
+    const [data, setData] = useState([]);
+    const [filterVal, setFilterVal] = useState("")
+    const [searchApiData, setSearchApiData] = useState([])
+    useEffect(() => {
+        const fetchData = () => {
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(response => response.json())
+                // .then(json => console.log("json", json))
+                .then(json => {
+                    setData(json)
+                    setSearchApiData(json)
+                })
+        }
+        fetchData()
+    }, [])
+
+    const handleFilter = (e) => {
+        if (e.target.value === '') {
+            setData(searchApiData)
+        } else {
+            let filterResult = searchApiData.filter(item => item.name.toLowerCase().includes(e.target.value.toLowerCase()))
+            setData(filterResult)
+        }
+        setFilterVal(e.target.value)
+
+    }
     return (
         <>
+            <div>
+                <div>
+                    <input placeholder='search' value={filterVal} onChange={(e) => {
+                        handleFilter(e)
+                    }} />
+                </div>
+                <table>
+                    <th>name</th>
+                    <th>username</th>
+                    <th>email</th>
+                    <th>phone</th>
+                    {
+                        data.map(item => {
+                            return (
+                                <tr>
+                                    <td>
+                                        {item.name}
+                                    </td>
+                                    <td>
+                                        {item.username}
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </table>
+            </div>
 
         </>
     )
